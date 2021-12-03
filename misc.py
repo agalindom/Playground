bfcc810cf01f745d8eade431c8a2cf5b93cc03c7
Alejandro Galindo <alegmed09@gmail.com>
2021-12-03 13:42:08 -0600

    Added two new functions to misc.py

--- misc.py
+++ misc.py
@@ -31,7 +31,57 @@ def sum_vals(int_1, int_2):
     return int_1 + int_2
 
 
+def divide_vals(numerator, denominator):
+    '''
+    Args:
+        numerator: (float) numerator of fraction
+        denominator: (float) denominator of fraction
+
+    Returns:
+        fraction_val: (float) numerator/denominator
+    '''
+    try:
+        assert isinstance(numerator, float)
+        assert isinstance(denominator, float)
+    except AssertionError:
+        logging.error("One or both input types are not float")
+    try:
+        assert denominator != 0
+        logging.info("%d/%d", numerator, denominator)
+        fraction_val = numerator / denominator
+        return fraction_val
+    except AssertionError:
+        logging.error("Denominator cannot be zero")
+
+    return None
+
+
+def num_words(text):
+    '''
+    Args:
+        text: (string) string of words
+
+    Returns:
+        num_words: (int) number of words in the string
+    '''
+    try:
+        assert isinstance(text, str)
+        logging.info("%s", text)
+        splited_words = len(text.split())
+        return splited_words
+    except AssertionError:
+        logging.error("Text argument must be a string")
+
+    return None
+
+
 if __name__ == "__main__":
     sum_vals('no', 'way')
-    sum_vals(3.4, 5)
-    sum_vals(4, 5)
+    sum_vals(3, 5)
+    divide_vals(3.4, 0)
+    divide_vals(4.5, 2.7)
+    divide_vals(-3.8, 2.1)
+    divide_vals(1, 2)
+    num_words(5)
+    num_words('This is the best string')
+    num_words('one')
